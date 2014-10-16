d6a59d392a26230abc854e521e14f7c9bcde7cc5
Leandro Bueno Santos <leandro.bueno@globo.com>
2014-10-16 13:46:59 -0400

    Refactor name variable better

--- lib/markup_calculator/markup.rb
+++ lib/markup_calculator/markup.rb
@@ -2,10 +2,10 @@
 class Markup
   attr_reader :base_price, :markup_policy
 	
-  def initialize(base_price, number_of_people, category)
-    validate_required_parameters base_price, number_of_people, category
+  def initialize(base_price, people_count, category)
+    validate_required_parameters base_price, people_count, category
     @base_price = base_price
-    @markup_policy = MarkupPolicy.new number_of_people, Category.new(category)
+    @markup_policy = MarkupPolicy.new people_count, Category.new(category)
   end 
   
   def total
@@ -25,9 +25,9 @@ private
     flat_percentage_total * markup_policy.percentage_for_category
   end
   
-  def validate_required_parameters(base_price, number_of_people, category)
+  def validate_required_parameters(base_price, people_count, category)
     fail 'Missing base price' if base_price.nil?
-    fail 'Missing number of people' if number_of_people.nil?
+    fail 'Missing number of people' if people_count.nil?
     fail 'Missing category' if category.nil?
   end
 end
